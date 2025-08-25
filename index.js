9664ecc092538e591b3b4277f9d64836ca2c9a5b
Kruppers67 <lyladanelleroberts@gmail.com>
2025-08-25 13:59:17 -0600

    Fixes to general layout, added a div to the main box.

---
+++ index.js
@@ -0,0 +1,5 @@
+const backgroundBox = document.getElementById('background-box');
+
+const gridContainer = document.createElement('div');
+gridContainer.className = 'grid-box';
+backgroundBox.appendChild(gridContainer);
